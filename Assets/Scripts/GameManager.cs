71e611c5b1ff704c69b092c04bccc9f7601a57a8
Martin Akula <martin.akula@gmail.com>
2021-09-21 14:26:15 +0300

    Upload
    upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+
+    private GroundPiece[] allGroundPieces;
+    private int sceneNumber;
+    private void Start()
+    {
+        SetupNewLevel();
+    }
+
+    private void SetupNewLevel()
+    {
+        allGroundPieces = FindObjectsOfType<GroundPiece>();
+    }
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+            NextLevel();
+    }
+
+    private void NextLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 7)
+        {
+            FirstLevel();
+        }
+        else
+        {
+            sceneNumber = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(sceneNumber);
+        }
+
+
+    }
+
+    private void FirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
